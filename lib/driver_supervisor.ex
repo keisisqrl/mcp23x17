bce5f2827bfd54e7626ae2ed53fe946b4a621bdb
Amy Wilhelm <keisisqrl@thexyin.sexy>
2017-12-19 17:17:47 -0800

    cleaned up unused reg_name from DriverRegistry

--- lib/driver_supervisor.ex
+++ lib/driver_supervisor.ex
@@ -2,13 +2,6 @@ defmodule Mcp23x17.DriverSupervisor do
   use Supervisor
   alias Mcp23x17.Driver
 
-  defmacro reg_name(name) do
-    quote do
-      {:via, Registry, {Mcp23x17.DriverRegistry, name}}
-    end
-  end
-  
-
   def start_link(_arg) do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
