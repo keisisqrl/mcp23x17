c4a2021dd560c3826dca5fac8bb56eade86c4f69
Amy Wilhelm <keisisqrl@thexyin.sexy>
2017-12-27 14:50:16 -0800

    Wrap lines

--- lib/adapter.ex
+++ lib/adapter.ex
@@ -5,10 +5,12 @@ defmodule Mcp23x17.Adapter do
     @moduledoc """
     Behaviour for Bus adapters (I2C or SPI).
     """
-    
-    @callback read(Mcp23x17.Driver.t, <<_::8>>, integer) :: binary() | {:error, term}
 
-    @callback write(Mcp23x17.Driver.t, << _::8 >>, binary) :: :ok | {:error, term}
+    @callback read(Mcp23x17.Driver.t, <<_::8>>, integer) ::
+    binary() | {:error, term}
+
+    @callback write(Mcp23x17.Driver.t, << _::8 >>, binary) ::
+    :ok | {:error, term}
   end
 
   defmodule Gpio do
@@ -20,7 +22,8 @@ defmodule Mcp23x17.Adapter do
 
     @callback write(GenServer.server, 0 | 1 | boolean) :: :ok | {:error, term}
     
-    @callback set_int(GenServer.server, Mcp23x17.Pin.interrupt_direction) :: :ok | {:error, term()}
+    @callback set_int(GenServer.server, Mcp23x17.Pin.interrupt_direction) ::
+    :ok | {:error, term()}
   end
     
 end
