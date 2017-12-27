edc1ed6194fba4f76c2344f87284cf6263d870a2
Amy Wilhelm <keisisqrl@thexyin.sexy>
2017-12-27 14:24:20 -0800

    ElixirALE.SPI adapter now calls Utils procs for address manipulation

--- lib/adapters/elixir_ale_spi.ex
+++ lib/adapters/elixir_ale_spi.ex
@@ -1,10 +1,12 @@
 defmodule Mcp23x17.Adapters.ElixirALE.SPI do
   alias ElixirALE.SPI
+  import Mcp23x17.Utils
   @behaviour Mcp23x17.Adapter.Bus
 
   def read(driver, regaddr, len) do
     case SPI.transfer(driver.xfer_pid,
-          << driver.addr::7, 1::1, regaddr::binary, 0::unit(8)-size(len) >>) do
+          << read_addr(driver.addr)::8,
+          regaddr::binary, 0::unit(8)-size(len) >>) do
       << _::16, retval::binary-size(len) >> ->
         retval
       {:error, err} ->
@@ -14,7 +16,8 @@ defmodule Mcp23x17.Adapters.ElixirALE.SPI do
 
   def write(driver, regaddr, data) do
     case SPI.transfer(driver.xfer_pid,
-          << driver.addr::7, 0::1, regaddr::binary, data::binary >>) do
+          << send_addr(driver.addr)::8,
+          regaddr::binary, data::binary >>) do
       {:error, err} ->
         {:error, err}
       << _::binary >> ->
